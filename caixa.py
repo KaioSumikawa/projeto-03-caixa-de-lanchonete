383afd577e9248f5ab39b9f63e5663be1cecb1b4
Kaio Sumikawa <kaiosumikawa@gmail.com>
2026-06-25 20:33:38 +0900

    feat: commit

--- caixa.py
+++ caixa.py
@@ -14,3 +14,13 @@ while True:
     qtd_itens += 1
     print(f'total atual: {total:.2f}')
 
+print(f'total final: {total:.2f}')
+print(f'quantidade itens: {qtd_itens}')
+pagar = float(input('digite valor pago: '))
+troco = pagar - total
+
+if troco < 0:
+    print("pagamento insuficiente")
+else:
+    print(f'troco: {troco:.2f}')
+
